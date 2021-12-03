a1f1a1457ddbe903c256fc6bc2fedc53b8c578a3
karishma Koladiya <karishma.koladiya@bacancy.com>
2021-12-03 14:06:03 +0530

    commit invoice listing update

--- app/Payments/Http/Controllers/Admin/IndexController.php
+++ app/Payments/Http/Controllers/Admin/IndexController.php
@@ -3,7 +3,8 @@ namespace App\Payments\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Validation\ValidationException;
+use Alert;
 class IndexController extends \WFN\Admin\Http\Controllers\Crud\Controller
 {
     
@@ -57,6 +58,42 @@ class IndexController extends \WFN\Admin\Http\Controllers\Crud\Controller
             'newlyweds' => [$customer->first_newlywed, $customer->second_newlywed],
             'contacts'  => $customer->contacts
         ];
+    }
+
+    public function save(Request $request)
+    {
+        // dd($request->all());
+        try {
+            if($request->input('id')) {
+                $this->entity = $this->entity->findOrFail($request->input('id'));
+            }
+
+            $this->validator($request->all())->validate();
+
+            $data = $this->_prepareData($request->all());
+            $this->entity->fill($data)->save();
+
+            $this->_afterSave($request);
+
+            Alert::addSuccess($this->entityTitle . ' has been saved');
+
+        } catch (ValidationException $e) {
+            foreach($e->errors() as $messages) {
+                foreach ($messages as $message) {
+                    Alert::addError($message);
+                }
+            }
+        } catch (\Exception $e) {
+            Alert::addError('Something went wrong. Please, try again');
+        }
+
+        if($request->btn_state == "Save & New"){
+            return redirect()->route($this->adminRoute . '.new',['customer_id'=>$request->customer_id]);
+
+        }else{
+            return !$this->entity->id ? redirect()->route($this->adminRoute . '.new') : redirect()->route($this->adminRoute . '.edit', ['id' => $this->entity->id]);
+
+        }
     }
     
 }
