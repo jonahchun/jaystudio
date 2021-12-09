153c63c4d06097ffbcf00aa8213616f077eb090a
karishma Koladiya <karishma.koladiya@bacancy.com>
2021-12-09 13:45:11 +0530

    commit wedding from point1 and point2

--- resources/views/customer/wedding/details.blade.php
+++ resources/views/customer/wedding/details.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 	<?php
+		// This wedding form is accessible only before 1 week and In between 4 months from wedding date
+		// If current date between 1 week of wedding date then not accessible and 
+		// If form is access before 4 month then not accessible
+
 		$before_date = date("Y-m-d H:i:s", strtotime ( '-4 month' , strtotime (Auth::user()->wedding_date) )) ;
 		
 		$readonly_flag = 'false';
 		if(Auth::user()->is_disable_update == 'Yes'){
 			$readonly_flag = 'true';
 		}else{
-			if(Auth::user()->wedding_date->subMonth('4')->gt(Illuminate\Support\Carbon::now())){
-				// dd('1');
+			if(Auth::user()->wedding_date->subWeek()->lt(Illuminate\Support\Carbon::now())){
 				$readonly_flag = 'true';
-			}elseif(Auth::user()->wedding_date->lt(Illuminate\Support\Carbon::now())){
-			// }elseif(Auth::user()->wedding_date->subWeek()->lt(Illuminate\Support\Carbon::now())){
-				// dd('2');
+			}elseif(Auth::user()->wedding_date->subMonth('4')->gt(Illuminate\Support\Carbon::now())){
 				$readonly_flag = 'true';
-			}
-			
 
+			}
 		}
 	?>
     <newlywed-details-form 
