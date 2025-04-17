9f8cb7e5538b85d047def94809c23d3412e3f7ec
Evgeny Monsvetov <emonsvetov@incento.com>
2025-04-17 17:39:34 -0500

    BTS - 12

---
+++ app/Observers/InvoiceObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Payments\Model\Invoice;
+use App\Payments\Model\Source\Status;
+use Illuminate\Support\Facades\Log;
+
+class InvoiceObserver
+{
+    public function updated(Invoice $invoice)
+    {
+        if ($invoice->isDirty('status') && (int)$invoice->status === Status::PAID) {
+            $this->sendPaymentReceivedEmail($invoice);
+        }
+    }
+
+    protected function sendPaymentReceivedEmail(Invoice $invoice)
+    {
+        $data = [
+            'first_newlywed_name'   => $invoice->customer->first_newlywed->first_name,
+            'second_newlywed_name'  => $invoice->customer->second_newlywed->first_name,
+            'id' => $invoice->id,
+            'amount' => $invoice->amount,
+        ];
+
+        Log::info('Email sent: "Payment received". Data: ' . print_r($data, true));
+
+        \MandrillMail::send('payment_received', $invoice->customer->email, $data);
+    }
+}
